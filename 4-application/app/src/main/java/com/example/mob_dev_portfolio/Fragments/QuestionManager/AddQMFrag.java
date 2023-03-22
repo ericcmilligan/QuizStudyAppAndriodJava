8ee8f42e612fd554309ccafac899aac692b0b966
c21040245 <milliganEC@cardiff.ac.uk>
2023-03-22 11:56:01 +0000

    Can go back to question manager fragment.

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/QuestionManager/AddQMFrag.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/QuestionManager/AddQMFrag.java
@@ -2,16 +2,26 @@ package com.example.mob_dev_portfolio.Fragments.QuestionManager;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import com.example.mob_dev_portfolio.R;
+import com.example.mob_dev_portfolio.databinding.FragmentAddQuestionBinding;
+import com.example.mob_dev_portfolio.databinding.FragmentHomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class AddQMFrag extends Fragment {
+
+    private FragmentAddQuestionBinding binding;
 
     public AddQMFrag() {
 
@@ -25,7 +35,19 @@ public class AddQMFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_question, container, false);
+        binding = FragmentAddQuestionBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        FloatingActionButton backToQMButton = (FloatingActionButton) root.findViewById(R.id.backToQMButton);
+
+        backToQMButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_nav_add_question_to_nav_question_manager);
+            }
+        });
+
+        return root;
     }
+
 }
--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/MainActivity.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/MainActivity.java
@@ -8,6 +8,11 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
 import android.view.Menu;
+
+import com.example.mob_dev_portfolio.Database.QuizDatabase;
+import com.example.mob_dev_portfolio.Entities.Answer;
+import com.example.mob_dev_portfolio.Entities.Question;
+import com.example.mob_dev_portfolio.Entities.Tag;
 import com.example.mob_dev_portfolio.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
 
