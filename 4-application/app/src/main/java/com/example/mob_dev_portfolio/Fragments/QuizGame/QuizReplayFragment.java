48212ca6d8025215e81dac70420c4c8373b2b4de
c21040245 <milliganEC@cardiff.ac.uk>
2023-04-03 14:41:57 +0100

    GIF retrieved from Giphy API with the GIF retrieved based on win/lose state of quiz.

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/QuizGame/QuizReplayFragment.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/QuizGame/QuizReplayFragment.java
@@ -11,14 +11,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.mob_dev_portfolio.API.DataModel;
+import com.example.mob_dev_portfolio.Database.QuizDatabase;
+import com.example.mob_dev_portfolio.Entities.Highscore;
 import com.example.mob_dev_portfolio.R;
 import com.example.mob_dev_portfolio.databinding.FragmentQuizReplayBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -28,6 +34,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * A fragment to display after the quiz game has finished that shows the user the high-score and
@@ -41,9 +48,9 @@ public class QuizReplayFragment extends Fragment {
     ArrayList<DataModel> dataModelArrayList = new ArrayList<>();
     public static final String API_KEY = "UGeUJXTeOyZZXtzZXFFD87f9HIMX2bOJ";
     public static final String BASE_URL = "https://api.giphy.com/v1/gifs/search?api_key=";
-    public static final String SEARCH_TERM = "&q=winning&limit=20&offset=0&rating=g&lang=en";
-
-    String url = BASE_URL + API_KEY + SEARCH_TERM;
+    public static final String BEFORE_SEARCH_URL = "&q=";
+    public static String SEARCH_TERM = "winning";
+    public static final String AFTER_SEARCH_URL = "&limit=20&offset=0&rating=g&lang=en";
 
     public QuizReplayFragment() {
 
@@ -71,7 +78,9 @@ public class QuizReplayFragment extends Fragment {
         String bundleTagIDInt = bundleReceivedTagIDString.replaceAll("\\D+","");
         Integer tagID = Integer.parseInt(bundleTagIDInt);
 
-
+        //Get high-score to load correct gif type
+        QuizDatabase db = QuizDatabase.getInstance(getActivity().getApplicationContext());
+        Integer quizHighScoreForTag = db.highScoreDao().getHighScorePointsByTagID(tagID);
 
         //Show the user the result of the quiz
         if(quizScore == null){
@@ -122,33 +131,15 @@ public class QuizReplayFragment extends Fragment {
             }
         });
 
-        //Getting the data from the API to display a winning or losing gif
-        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    JSONArray dataArray = response.getJSONArray("data");
-                    for (int i = 0; i < dataArray.length(); i++) {
-                        JSONObject obj = dataArray.getJSONObject(i);
-                        JSONObject obj1 = obj.getJSONObject("images");
-                        JSONObject obj2 = obj1.getJSONObject("downsized_medium");
+        //Set up request queue for Giphy API
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
 
-                        String sourceUrl = obj2.getString("url");
-
-                        dataModelArrayList.add(new DataModel(sourceUrl));
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-              }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(getContext(), "Error occurred while getting gif: " +
-                            error.getMessage(),
-                            Toast.LENGTH_SHORT).show();
-                }
-        });
+        //Set gif retrieved from API depending on winning/losing condition state
+        if(quizScore > quizHighScoreForTag | quizScore.equals(quizHighScoreForTag)){
+            getGif(requestQueue, "win");
+        } else {
+            getGif(requestQueue, "lose");
+        }
 
         //Go back to quiz start screen on back press
         OnBackPressedCallback callback = new OnBackPressedCallback(true) {
@@ -164,6 +155,52 @@ public class QuizReplayFragment extends Fragment {
         requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
 
         return root;
+    }
+
+    public void getGif(RequestQueue requestQueue, String searchTerm){
+        //Getting the data from the API to display a gif for either winning or losing
+        SEARCH_TERM = searchTerm;
+        String url = BASE_URL + API_KEY + BEFORE_SEARCH_URL + SEARCH_TERM  + AFTER_SEARCH_URL;
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray dataArray = response.getJSONArray("data");
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        JSONObject obj = dataArray.getJSONObject(i);
+                        JSONObject obj1 = obj.getJSONObject("images");
+                        JSONObject obj2 = obj1.getJSONObject("downsized_medium");
+
+                        String sourceUrl = obj2.getString("url");
+
+                        dataModelArrayList.add(new DataModel(sourceUrl));
+                    }
+
+                    //Get random gif image url for the array
+                    Collections.shuffle(dataModelArrayList);
+
+                    //Load gif into the image view with glide
+                    ImageView quizResultImage = getActivity().findViewById(R.id.quiz_replay_image_view);
+
+                    Glide.with(getContext())
+                            .asGif()
+                            .load(dataModelArrayList.get(1).getImageUrl())
+                            .into(quizResultImage);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "Error occurred while getting gif, not loaded" +
+                                error.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(objectRequest);
     }
 
     @Override
