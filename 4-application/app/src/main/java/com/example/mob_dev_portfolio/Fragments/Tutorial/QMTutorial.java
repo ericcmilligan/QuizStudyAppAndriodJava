71946dbc8fe98ff0d3cacb44d5dc669d71435783
c21040245 <milliganEC@cardiff.ac.uk>
2023-04-14 15:41:20 +0100

    Can play question manager tutorial GIF on question manager tutorial fragment.

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/Tutorial/QMTutorial.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/Tutorial/QMTutorial.java
@@ -7,7 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.example.mob_dev_portfolio.R;
 import com.example.mob_dev_portfolio.databinding.FragmentQuestionManagerTutorialBinding;
 
 /**
@@ -32,6 +35,14 @@ public class QMTutorial extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentQuestionManagerTutorialBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        ImageView questionMangerTutorialGif = binding.questionManagerTutorialImageView;
+
+        Glide.with(getContext())
+                .asGif()
+                .load(R.drawable.qmtutorial)
+                .placeholder(R.drawable.placeholder)
+                .into(questionMangerTutorialGif);
 
         return root;
     }
