3a27e87db26fd997f7e3d7d6a33568cb88cf1692
c21040245 <milliganEC@cardiff.ac.uk>
2023-04-18 16:46:24 +0100

    Can go to next app tutorial within tutorial section.

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/Tutorial/QGTutorial.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/Tutorial/QGTutorial.java
@@ -1,5 +1,6 @@
 package com.example.mob_dev_portfolio.Fragments.Tutorial;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mob_dev_portfolio.databinding.FragmentQuizGameTutorialBinding;
 
@@ -25,6 +28,9 @@ public class QGTutorial extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Fragment locked in portrait screen orientation as the tutorial does not display correctly
+        //in landscape mode
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     @Override
@@ -33,6 +39,21 @@ public class QGTutorial extends Fragment {
         binding = FragmentQuizGameTutorialBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        ImageView quizGameTutorialGIF = binding.quizGameTutorialImageView;
+
+        Toast.makeText(getContext(), "Tutorial displays in vertical orientation only",
+                Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "Click the back arrow to go to the previous tutorial for the app",
+                Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "Click the front arrow to go the next tutorial for the app",
+                Toast.LENGTH_SHORT).show();
+
         return root;
+    }
+
+    public void onDestroyView() {
+        super.onDestroyView();
+        //Set orientation back to landscape and vertical after leaving this fragment
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
     }
 }
--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/Tutorial/QMTutorial.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/Tutorial/QMTutorial.java
@@ -1,9 +1,11 @@
 package com.example.mob_dev_portfolio.Fragments.Tutorial;
 
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.mob_dev_portfolio.R;
 import com.example.mob_dev_portfolio.databinding.FragmentQuestionManagerTutorialBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * This fragment contains the question manager tutorial to guide the user on how managing questions
@@ -55,6 +58,43 @@ public class QMTutorial extends Fragment {
                 Toast.LENGTH_SHORT).show();
         Toast.makeText(getContext(), "Click the home button to return home",
                 Toast.LENGTH_SHORT).show();
+
+        //Set up button for going back to the home screen from the tutorial
+        FloatingActionButton qmTutHomeButton = binding.quitQuestionManagerTutorialButton;
+        qmTutHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer buttonClickSound = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.click);
+
+                if(buttonClickSound != null){
+                    buttonClickSound.start();
+                    if(!buttonClickSound.isPlaying()){
+                        buttonClickSound.release();
+                    }
+                }
+
+                Navigation.findNavController(v).navigate(R.id.action_nav_question_manager_tutorial_to_nav_home);
+            }
+        });
+
+        //Set up button for going to the next tutorial screen
+        FloatingActionButton qmTutNextTutButton = binding.questionManagerNextTutorialButton;
+        qmTutNextTutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer buttonClickSound = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.click);
+
+                if(buttonClickSound != null){
+                    buttonClickSound.start();
+                    if(!buttonClickSound.isPlaying()){
+                        buttonClickSound.release();
+                    }
+                }
+
+                Navigation.findNavController(v).navigate(R.id.action_nav_question_manager_tutorial_to_nav_quiz_game_tutorial);
+            }
+        });
+
 
         return root;
     }
