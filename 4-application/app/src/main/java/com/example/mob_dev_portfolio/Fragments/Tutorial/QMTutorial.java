d7d0629d2dac502b0d714e35b1cd025739ec8544
c21040245 <milliganEC@cardiff.ac.uk>
2023-04-18 15:02:38 +0100

    Can display question manager GIF tutorial correctly.

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/Tutorial/QMTutorial.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/Fragments/Tutorial/QMTutorial.java
@@ -1,5 +1,6 @@
 package com.example.mob_dev_portfolio.Fragments.Tutorial;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.mob_dev_portfolio.R;
@@ -28,6 +30,9 @@ public class QMTutorial extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Fragment locked in portrait screen orientation as the tutorial does not display correctly
+        //in landscape mode
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     @Override
@@ -44,6 +49,19 @@ public class QMTutorial extends Fragment {
                 .placeholder(R.drawable.placeholder)
                 .into(questionMangerTutorialGif);
 
+        Toast.makeText(getContext(), "Tutorial displays in vertical orientation only",
+                Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "Click the next arrow to go to the next tutorial for the app",
+                Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "Click the home button to return home",
+                Toast.LENGTH_SHORT).show();
+
         return root;
+    }
+
+    public void onDestroyView() {
+        super.onDestroyView();
+        //Set orientation back to landscape and vertical after leaving this fragment
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
     }
 }
