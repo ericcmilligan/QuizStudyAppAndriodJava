cf2904e733605f396a510320ef02f634e3d49ae4
c21040245 <Robofudgecaruni@123>
2023-10-04 15:03:43 +0100

    Added improved javadoc comments to data model class.

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/API/DataModel.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/API/DataModel.java
@@ -1,19 +1,34 @@
 package com.example.mob_dev_portfolio.API;
 
 /**
- * A data model class for saving the giphy API response into.
+ * This class represents a data model for saving the Giphy API image url response into.
  */
 public class DataModel {
+    // Private field to store the image URL
     private String imageUrl;
 
+    /**
+     * Constructor to initialize a DataModel object with the image URL as a property.
+     * @param imageUrl
+     */
     public DataModel(String imageUrl) {
         this.imageUrl = imageUrl;
     }
 
+    /**
+     * Getter method to retrieve the image URL.
+     *
+     * @return the image URL as a string from the data model
+     */
     public String getImageUrl() {
         return imageUrl;
     }
 
+    /**
+     * Setter method to set the image URL.
+     *
+     * @param the image URL to set
+     */
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
