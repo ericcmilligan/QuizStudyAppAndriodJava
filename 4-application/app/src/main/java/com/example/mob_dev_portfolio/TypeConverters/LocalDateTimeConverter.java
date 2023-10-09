3fba9d1fd128ea109655cee546072bcdc222b497
c21040245 <Robofudgecaruni@123>
2023-10-09 15:07:44 +0100

    Improved JavaDoc comments for the local date time converter.

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/TypeConverters/LocalDateTimeConverter.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/TypeConverters/LocalDateTimeConverter.java
@@ -8,10 +8,17 @@ import androidx.room.TypeConverter;
 import java.time.LocalDateTime;
 
 /**
- * A class to convert the time format between java and the room database.
+ * A class to convert LocalDateTime objects to and from String format for enabling storage of date
+ * within the room database.
  */
 public class LocalDateTimeConverter {
 
+    /**
+     * Converts a String representation of date and time to a LocalDateTime object.
+     *
+     * @param dateString The String representation of the date and time (ISO-8601 format)
+     * @Return A LocalDateTime object representing the date and time.
+     */
     @TypeConverter
     public static LocalDateTime toDate(String dateString) {
         if (dateString == null) {
@@ -21,6 +28,12 @@ public class LocalDateTimeConverter {
         }
     }
 
+    /**
+     * Converts a LocalDateTime object to its String representation.
+     *
+     * @param date The LocalDateTime object to be converted
+     * @Return A String representation of the LocalDateTime (ISO-8601 format)
+     */
     @TypeConverter
     public static String toDateString(LocalDateTime date) {
         if (date == null) {
